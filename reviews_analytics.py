c6ddb1d191074dea8850e64a69361f38fe71080e
Austin PC Chen <atn920210@gmail.com>
2021-04-17 16:47:21 +0800

    add sum & average

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -2,12 +2,14 @@ data = []
 count = 0
 with open('reviews.txt','r') as f:
 	for line  in f:
-		data.append(line)
+		data.append(line.strip())
 		count += 1 # count = count + 1
 		if count % 1000 == 0: # %求餘數 (除以 1000 餘數為 0)
 			print(len(data))
-print(len(data))
+print('檔案讀取完了, 總共有',len(data), '筆資料')
 
-print(data[0])
-print('-----------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言平均長度為', sum_len/len(data))
